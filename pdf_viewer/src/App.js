27513ffeab1b088023a4903aee7586b7564d7531
Mohammad Saif <saifsami321md@gmail.com>
2022-12-05 00:59:25 +0530

    error occure while click on show pdf

--- pdf_viewer/src/App.js
+++ pdf_viewer/src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
+
 import './App.css';
+import PdfViewer from './PdfViewer';
 
 function App() {
   return (
     <div className="App">
       <h1>Basic SetUp Ready</h1>
+      <PdfViewer/>
     </div>
   );
 }
